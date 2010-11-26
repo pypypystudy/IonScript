13beb44ad2b6749017e0bad19386a7c91a392461
Canio Massimo Tristano <massimo.tristano@gmail.com>
2010-11-26 01:30:51 +0100

    Jeez! Foo must inherit from IManageableObject since it's managed in the example (I could also use the new Managed<T> class to manage objects that do not implement that interface, but for clarity I sticked with the former).

--- example/source/Foo.h
+++ example/source/Foo.h
@@ -1,32 +1,31 @@
 #ifndef FOO_H
 #define FOO_H
 
+#include "IonScript/IonScript.h"
+
 #include <string>
 #include <vector>
 
 class Boo;
 
-class Foo {
+class Foo : public ion::script::IManageableObject {
 public:
-	Foo (const std::string& name, size_t childrenCount);
-	
-	virtual ~Foo();
-	
-	const std::string& getName() const {
-		return mName;
-	}
-	
-	void sayHello() const;
-	
-	Boo* getBoo(size_t index) {
-		return mChildren[index];
-	}
-	
-	Boo* createBoo();
-	
+   Foo (const std::string& name, size_t childrenCount);
+   virtual ~Foo ();
+   const std::string& getName () const {
+      return mName;
+   }
+
+   void sayHello () const;
+   Boo* getBoo (size_t index) {
+      return mChildren[index];
+   }
+
+   Boo* createBoo ();
+
 private:
-	std::string mName;
-	std::vector<Boo*> mChildren;
+   std::string mName;
+   std::vector<Boo*> mChildren;
 };
 
 #endif
